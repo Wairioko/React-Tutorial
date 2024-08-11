6cccfafacb44f799142be2ec7cf33ec2d1c85d81
Wairioko <charlesmungai5@gmail.com>
2024-08-11 10:24:36 +0300

    INtroduction to using state in react, useState hook illustrated

--- src/Home.js
+++ src/Home.js
@@ -1,22 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './index.css'
 
-const onclick = () => {
-    alert('Home button clicked');
-}
+
 
 const clickagain = (name) => {
+
     alert('Hello' + name)
 }
 
 // Home.js
 const HomePage = () => {
+    // let name = 'munga'
+    // use of state functionality in react
+    const [name, setName] = useState('munga');
+
+    const onclick = () => {
+        setName('charles')
+        alert('Home button clicked by ' + name);
+    }
     return (  
         <div className="content">
             <h2>Home Page</h2>
             <button onClick={onclick}>Click me</button>
             {/* wrapping in anonymous function so it can be 
             called properly when button clicked*/}
+            <p>{name}</p>
             <button onClick={(e) => {clickagain('munga', e)}}>Click me</button>
         </div>
     );
