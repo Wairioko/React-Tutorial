2c90552dae07f3b8b359c81c93be67a78713af8b
Wairioko <charlesmungai5@gmail.com>
2024-08-11 10:11:33 +0300

    introduction to click events and functions

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,23 @@
 import React from 'react'
 import './index.css'
+
+const onclick = () => {
+    alert('Home button clicked');
+}
+
+const clickagain = (name) => {
+    alert('Hello' + name)
+}
+
 // Home.js
 const HomePage = () => {
     return (  
         <div className="content">
             <h2>Home Page</h2>
+            <button onClick={onclick}>Click me</button>
+            {/* wrapping in anonymous function so it can be 
+            called properly when button clicked*/}
+            <button onClick={(e) => {clickagain('munga', e)}}>Click me</button>
         </div>
     );
 }
