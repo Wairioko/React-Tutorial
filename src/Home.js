ed3d985a867e3cf0373406a6b613307381b0cced
Wairioko <charlesmungai5@gmail.com>
2024-08-11 09:28:51 +0300

    Homepage content component set up

---
+++ src/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './index.css'
+// Home.js
+const HomePage = () => {
+    return (  
+        <div className="content">
+            <h2>Home Page</h2>
+        </div>
+    );
+}
+
+export default HomePage;
+
+
