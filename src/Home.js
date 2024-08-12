5ef3d606a05b58d0a68dc2a72727cda14e97685a
Wairioko <charlesmungai5@gmail.com>
2024-08-12 13:40:54 +0300

    Imported blog component and created blogs property to hold blogs data

--- src/Home.js
+++ src/Home.js
@@ -1,35 +1,56 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react';
 import './index.css'
-
+import BlogList from './Blog';
 
 
 const clickagain = (name) => {
-
-    alert('Hello' + name)
+    alert('Hello ' + name);
 }
 
 // Home.js
 const HomePage = () => {
-    // let name = 'munga'
-    // use of state functionality in react
+    // useState hook to manage state in functional components
+    // lists and string components example
     const [name, setName] = useState('munga');
+    const [blogs, setBlogs] = useState([
+        {title: 'Blog 1', content: 'This is blog 1 content'},
+        {title: 'Blog 2', content: 'This is blog 2 content'},
+        {title: 'Blog 3', content: 'This is blog 3 content'},
+    ]);
 
-    const onclick = () => {
-        setName('charles')
-        alert('Home button clicked by ' + name);
+    //delete function through filtering illustrated
+    const DeleteBlog = (blogTitle) => {
+        const updatedBlogs = blogs.filter((blog) => blog.title !== blogTitle)
+        setBlogs(updatedBlogs);
+    
     }
+    
+    // function to run for every render
+    // empty array ensures that function runs only once
+    useEffect(() => {
+        console.log("Use effect run")
+        console.log(blogs);
+    }, []);
+
+    // This function will be called when the button is clicked
+    const onclick = () => {
+        setName('charles');
+        alert('Home button clicked by ' + name);
+    };
+
     return (  
         <div className="content">
-            <h2>Home Page</h2>
+            {/* functions and events in react */}
             <button onClick={onclick}>Click me</button>
-            {/* wrapping in anonymous function so it can be 
-            called properly when button clicked*/}
             <p>{name}</p>
-            <button onClick={(e) => {clickagain('munga', e)}}>Click me</button>
+            {/* prop functionality in react in blogs list data*/}
+            {/* functions as props */}
+            <BlogList blogs={blogs} DeleteBlog = {DeleteBlog}/>
+            {/*simple illustration of search page */}
+            {/* <BlogList blogs={blogs.filter((blog) => blog.title.includes('Blog 2'))}/> */}
+            <button onClick={() => clickagain('munga')}>Click me</button>
         </div>
     );
 }
 
 export default HomePage;
-
-
